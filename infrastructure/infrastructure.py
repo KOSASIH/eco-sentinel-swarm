4e26a32122f43e56984b618b0d614872765feb48
KOSASIH <kosasihg88@gmail.com>
2024-07-05 19:14:36 +0700

    Create infrastructure.py

---
+++ infrastructure/infrastructure.py
@@ -0,0 +1,27 @@
+import os
+import random
+from config import Config
+
+class Cloud:
+    def __init__(self, config):
+        self.config = config
+
+    def init(self):
+        # Initialize cloud infrastructure
+        pass
+
+    def start(self):
+        # Manage cloud infrastructure
+        pass
+
+class OnPrem:
+    def __init__(self, config):
+        self.config = config
+
+    def init(self):
+        # Initialize on-premises infrastructure
+        pass
+
+    def start(self):
+        # Manage on-premises infrastructure
+        pass
